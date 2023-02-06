691d68aa3021ea5c5e23ad0ea944b29ed5581ce9
Anushka Yadav <113161007+Anushka-Yadav@users.noreply.github.com>
2023-02-06 16:24:43 +0000

    first commit

---
+++ recurr.java
@@ -0,0 +1,26 @@
+// A Java program to demonstrate
+// working of recursion
+
+class recurr {
+	static void printFun(int test)
+	{
+		if (test < 1)
+			return;
+
+		else {
+			System.out.println(test);
+
+			// Statement 2
+			printFun(test - 1);
+
+			System.out.println(test);
+			return;
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		int test = 3;
+		printFun(test);
+	}
+}
