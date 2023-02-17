10348f0295227fc117c3f92156c80b5f8b5b94db
Anushka Yadav <113161007+Anushka-Yadav@users.noreply.github.com>
2023-02-17 10:18:14 +0000

    first commit

---
+++ TypeCast.java
@@ -0,0 +1,9 @@
+public class TypeCast{
+    public static void main(String arg[]){
+        short a = 10;
+        short b = 10;
+        a = (short)
+        (a+b);
+        System.out.println(a);
+    }
+}
