edb99fdb50c27c97a5e5bc803d30f0b143792c49
Anushka Yadav <113161007+Anushka-Yadav@users.noreply.github.com>
2023-02-18 13:24:48 +0000

    first commit

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class LeapYear {
     public static boolean isLeap(int n) {
-
+        
         if(n%4 == 0 ){
             if(n%100 == 0 && n%400 == 0){
                 return true;
